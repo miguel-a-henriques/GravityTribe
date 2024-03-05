062b60dd4cb8b0af957c79c3a54176d9be1e7b36
Filipe Teixeira <filipe942001teixeira@gmail.com>
2024-03-05 23:20:35 +0000

    Added a Dummy Logo to work on.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div>
-        <img src="src/images/handstand.png"/>
+        <img src="src/images/GT_LOGO.png" style={{width: "80px"}}/>
         <button>Login</button>
     </div>
   )
