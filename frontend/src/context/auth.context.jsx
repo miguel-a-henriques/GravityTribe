c986b137ed348f329034463ca3857a0bb93b8bda
Filipe Teixeira <filipe942001teixeira@gmail.com>
2024-03-06 11:34:32 +0000

    Added the context file

---
+++ frontend/src/context/auth.context.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+// Initializing context
+const AuthContext = React.createContext();
+
+const API_URL = "http://localhost:5005";
+
+function AuthProviderWrapper(props) {
+  const [user, setUser] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  /* Save the Login's JWT token in our Browser's Storage */
+  const saveToken = (token) => {
+    localStorage.setItem("authToken", token);
+  };
+
+  /* Function that authenticates the user ---> verifies if the token is valid */
+  const authenticateUser = () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    if (storedToken) {
+      axios
+        .get(`${API_URL}/auth/verify`, {
+          headers: {
+            Authorization: `Bearer ${storedToken}`,
+          },
+        })
+        .then((response) => {
+          setUser(response.data);
+          setIsLoggedIn(true);
+        })
+        .catch(() => {
+          setUser(null);
+          setIsLoggedIn(false);
+        });
+    } else {
+      setUser(null);
+      setIsLoggedIn(false);
+    }
+
+    const removeToken = () => {
+      localStorage.removeItem("authToken");
+    };
+
+    const logOut = () => {
+      removeToken();
+      authenticateUser();
+    };
+
+    return (
+      <AuthContext.Provider value={{ isLoggedIn, user, saveToken, authenticateUser, logOut }}>
+        {props.children}
+      </AuthContext.Provider>
+    );
+  };
+}
+
+export {AuthProviderWrapper, AuthContext};
