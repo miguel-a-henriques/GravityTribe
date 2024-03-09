72ca1443d85742cb133c4a84933434d6fefb31fa
Miguel Henriques <miguelaghenriques@gmail.com>
2024-03-09 18:54:33 +0000

    userprofile page

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,8 +1,34 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
+import axios from "axios";
+
+
+const API_URL = "http://localhost:5005";
+
 
 function UserProfile() {
+
+    const { user, isLoggedIn } = useContext(AuthContext);
+    const [ourUser, setOurUser] = useState({});
+
+
+
+    useEffect(()=>{
+        axios.get(`${API_URL}/api/user/${user._id}`)
+        .then((response)=>{
+            setOurUser(response.data)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    },[])
+
     return (
         <div>
-            UserProfile
+            <h2>{ourUser.name}</h2>
         </div>
     )
 }
