c51b1690d4084b9b155dcf5e6f3fb2e0c6d847b7
Filipe Teixeira <filipe942001teixeira@gmail.com>
2024-03-09 13:48:58 +0000

    Added the toggle filters for the buttons in the workouts list page

--- frontend/src/components/WorkoutsList.jsx
+++ frontend/src/components/WorkoutsList.jsx
@@ -1,48 +1,70 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import WorkoutsCard from "./WorkoutsCard";
 import { useNavigate } from "react-router-dom";
 
 const API_URL = "http://localhost:5005";
 
 function WorkoutsList() {
-    const [workouts, setWorkouts] = useState("")
-    const navigate = useNavigate()
+  const [workouts, setWorkouts] = useState("");
+  const [selectedDifficulty, setSelectedDifficulty] = useState(null);
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        axios.get(`${API_URL}/api/workouts`)
-        .then((response)=>{
-            setWorkouts(response.data)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
-    }, [])
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/api/workouts`)
+      .then((response) => {
+        setWorkouts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    return (
-        <div>
-            <section id={"skillsLevelsButtons"}>
-                <section>
-                    <button>Beginner</button>
-                    <button>Intermediate</button>
-                    <button>Advanced</button>
-                    <button>Master</button>
-                </section>
-                
-                <button onClick={()=>{navigate("/workouts/create")}}>
-                    Create Your Workout
-                </button>
-            </section>
-            <section>
-                {workouts && workouts.map((workout)=>{
-                    return(
-                        <WorkoutsCard workout={workout} id={workout._id}/>
-                    )
-                })}
-            </section>
-        </div>
-    )
+  const filterWorkoutsByDifficulty = (difficulty) => {
+    if (selectedDifficulty === difficulty) {
+      setSelectedDifficulty(null);
+    } else {
+      setSelectedDifficulty(difficulty);
+    }
+  };
+
+  const filteredWorkouts = selectedDifficulty ? workouts.filter((workout) => workout.expLevel === selectedDifficulty) : workouts;
+
+  return (
+    <div>
+      <section id={"skillsLevelsButtons"}>
+        <section>
+          <button onClick={() => filterWorkoutsByDifficulty("beginner")}>
+            Beginner
+          </button>
+          <button onClick={() => filterWorkoutsByDifficulty("intermediate")}>
+            Intermediate
+          </button>
+          <button onClick={() => filterWorkoutsByDifficulty("advanced")}>
+            Advanced
+          </button>
+          <button onClick={() => filterWorkoutsByDifficulty("master")}>
+            Master
+          </button>
+        </section>
+
+        <button
+          onClick={() => {
+            navigate("/workouts/create");
+          }}
+        >
+          Create Your Workout
+        </button>
+      </section>
+      <section>
+        {filteredWorkouts &&
+          filteredWorkouts.map((workout) => {
+            return <WorkoutsCard workout={workout} id={workout._id} />;
+          })}
+      </section>
+    </div>
+  );
 }
 
 export default WorkoutsList;
